c48c20a0a7436175ca393371005f3038dcc624db
humbertolopez <humberto@373.mx>
2015-02-23 13:28:03 -0600

    Adición de la página especial “Industrial”

---
+++ page-industrial.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Industrial
+*/
+get_header(); ?>
+
+<!-- cuerpo -->
+<div id="post" class="block">
+
+	<!-- inicia loop -->
+	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+			<h1><?php the_title(); ?></h1>		
+
+		<div class="doblecol firstcol">
+			<?php the_post_thumbnail('medium'); ?>
+
+			<?php the_content();?>
+
+			<a href="<?php bloginfo('url'); ?>/cotizacion"><h4 class="cotbutton">Solicita una cotización</h4></a>
+
+			<?php get_template_part('info-pagos'); ?>
+
+			<?php get_template_part('info-tarjetas'); ?>
+
+			<?php get_template_part('tablacalculo'); ?>
+
+		</div>
+		<div class="col">
+			<h3>Dimensiones</h3>
+				<?php get_template_part('dimensiones'); ?>
+		</div>
+
+	<?php endwhile; endif; ?>
+	<!-- fin loop -->
+
+</div>
+
+<?php get_footer(); ?>
